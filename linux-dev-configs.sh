e596c98a3f210fa9dc56ccdca07d282352926603
Nguyễn Công Quyền <quyen19492@gmail.com>
2025-11-19 05:29:22 +0700

    Add build profile for linux

---
+++ linux-dev-configs.sh
@@ -0,0 +1,59 @@
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster
+cd /mnt/k/git/ElefantBlaster
+git worktree add ~/projects/ElefantBlaster wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/ElefantBlasterClient
+cd /mnt/k/git/ElefantBlaster/ElefantBlasterClient
+git worktree add ~/projects/ElefantBlaster/ElefantBlasterClient wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/ElefantBlasterServer
+cd /mnt/k/git/ElefantBlaster/ElefantBlasterServer
+git worktree add ~/projects/ElefantBlaster/ElefantBlasterServer wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/abseil-cpp
+cd /mnt/k/git/ElefantBlaster/libs/abseil-cpp
+git worktree add ~/projects/ElefantBlaster/libs/abseil-cpp wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/aries_base
+cd /mnt/k/git/ElefantBlaster/libs/aries_base
+git worktree add ~/projects/ElefantBlaster/libs/aries_base wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/asio
+cd /mnt/k/git/ElefantBlaster/libs/asio
+git worktree add ~/projects/ElefantBlaster/libs/asio wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/glm
+cd /mnt/k/git/ElefantBlaster/libs/glm
+git worktree add ~/projects/ElefantBlaster/libs/glm wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/json
+cd /mnt/k/git/ElefantBlaster/libs/json
+git worktree add ~/projects/ElefantBlaster/libs/json wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/protobuf
+cd /mnt/k/git/ElefantBlaster/libs/protobuf
+git worktree add ~/projects/ElefantBlaster/libs/protobuf wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/SDL
+cd /mnt/k/git/ElefantBlaster/libs/SDL
+git worktree add ~/projects/ElefantBlaster/libs/SDL wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/SDL_image
+cd /mnt/k/git/ElefantBlaster/libs/SDL_image
+git worktree add ~/projects/ElefantBlaster/libs/SDL_image wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/SDL_mixer
+cd /mnt/k/git/ElefantBlaster/libs/SDL_mixer
+git worktree add ~/projects/ElefantBlaster/libs/SDL_mixer wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/SDL_net
+cd /mnt/k/git/ElefantBlaster/libs/SDL_net
+git worktree add ~/projects/ElefantBlaster/libs/SDL_net wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/SDL_ttf
+cd /mnt/k/git/ElefantBlaster/libs/SDL_ttf
+git worktree add ~/projects/ElefantBlaster/libs/SDL_ttf wsl
+
+git config --global --add safe.directory /mnt/k/git/ElefantBlaster/libs/websocketpp
+cd /mnt/k/git/ElefantBlaster/libs/websocketpp
+git worktree add ~/projects/ElefantBlaster/libs/websocketpp wsl
